b81617672564c652eabfe80a00f8681e35009eb7
alanpinhel <alanpinhel@hotmail.com>
2016-11-10 12:41:43 +0000

    Implementado Log de Requisições, Processamento Paralelo com Cluster, Compactação com GZip, Configuração de SSL Fictício e Configurações de Segurança com Helmet

--- libs/boot.js
+++ libs/boot.js
@@ -1,7 +1,22 @@
+import https from "https";
+import fs from "fs";
+
 module.exports = app => {
 	if (process.env.NODE_ENV !== "test") {
+		/**
+		 * Modelo de Implementação HTTPS
+		 *	const credenciais = {
+		 *		key: fs.readFileSync("ntask.key", "utf8"),
+		 *		cert: fs.readFileSync("ntask.cert", "utf8")
+		 *	};
+		 * 
+		 *	https.createServer(credenciais, app)
+		 *		.listen(app.get("porta"), () => {
+		 *			console.log(`NTask API - porta: ${app.get("porta")}`);
+		 *		});
+		 */
 		app.listen(app.get("porta"), () => {
-			console.log(`NTask API - porta: ${app.get("porta")}`);
+			console.log(`NTask API - porta: ${app.get("porta")}`); 		
 		});
 	}
 };
